aff2d6f00b65b3d611955768977a3db256ae51ea
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-11-09 23:30:58 +0100

    Added header

---
+++ components/header/NavLink.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link, { LinkProps } from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
+
+interface NavLinkProps extends LinkProps {
+	children: ReactNode;
+	className?: string;
+}
+
+export default function NavLink({ href, children, className, ...props }: NavLinkProps) {
+	const path = usePathname();
+	const isActive = path === href || path.startsWith(href + '/');
+
+	return (
+		<Link href={href} className={`${isActive ? 'text-tertiary' : 'text-primary'} ${className || ''}`.trim()} {...props}>
+			{children}
+		</Link>
+	);
+}
