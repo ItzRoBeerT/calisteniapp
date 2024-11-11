0820e6ab45530d29315d6f74499e230fbc1d31cd
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-11-11 12:28:30 +0100

    Fixed home styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Header from '@/components/header/Header'
-import Footer from '@/components/footer/Footer'
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Calistenia',
-  description: 'Aprende calistenia con nosotros y con nuestra comunidad',
-}
+	title: 'Calistenia',
+	description: 'Aprende calistenia con nosotros y con nuestra comunidad',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Header />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
