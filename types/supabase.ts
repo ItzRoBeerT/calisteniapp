2bb0b153abf371270649d76528154116387b6b27
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-11-12 14:53:50 +0100

    Added types for user, exercise and workout

---
+++ types/supabase.ts
@@ -0,0 +1,34 @@
+export interface Exercise {
+	id: number;
+	title: string;
+	description: string;
+	image: string;
+	video?: string;
+	likes: number[];
+	comments?: string[];
+	level: number;
+	family: string;
+	materials?: string[];
+	muscle: string;
+	muscle_group?: string;
+}
+
+export interface User {
+	id: number;
+	username: string;
+	email: string;
+	password: string;
+	google?: boolean;
+	follow: number[];
+}
+
+export interface Workout{
+	id: number;
+	owner: number;
+	likes: number[];
+	description?: string;
+	title: string;
+	image: string;
+	comments?: string[];
+	visible: boolean;
+}
