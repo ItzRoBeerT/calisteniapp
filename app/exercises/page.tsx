b84dff8f693182e592c3c206b58f9978f2af0334
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-11-14 11:24:36 +0100

    Added exercise list page

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,10 +1,30 @@
-import { getExercise } from "@/actions/exercise";
+import { getExercises } from '@/actions/exercise';
+import ExerciseCard from '@/components/exercises/card';
+import { Metadata } from 'next';
 
-export default async function Exercises() {
-	await getExercise(2);
+export const metadata: Metadata = {
+	title: 'Ejercicios',
+	description:
+		'Busca y encuentra los ejercicios que necesitas para mejorar tu fuerza y flexibilidad',
+};
+
+export default async function ExercisesPage() {
+	let exercises = (await getExercises()) || [];
+	console.log(exercises);
+
 	return (
 		<>
-			<h1>Ejercicios</h1>
+			<h1 className="text-4xl text-center font-bold">
+				Listado De Ejercicios
+			</h1>
+			<section>
+				<p>Filtros</p>
+			</section>
+			<section className="grid gap-4 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 my-4">
+				{exercises.map((exercise, index) => (
+					<ExerciseCard exercise={exercise} key={index} />
+				))}
+			</section>
 		</>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 		<html lang="en" className="min-h-full relative">
 			<body className={inter.className}>
 				<Header />
-				{children}
+				<main className="container mx-auto my-4">{children}</main>
 				<Footer />
 			</body>
 		</html>
