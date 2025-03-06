d156aa9921488f5d5a4bdfbba273aa2646d90b46
RafaelJaime <rajaimor@gmail.com>
2025-03-06 10:51:48 +0100

    compiling

--- app/[locale]/exercises/[slug]/page.tsx
+++ app/[locale]/exercises/[slug]/page.tsx
@@ -10,6 +10,7 @@ export default async function Page({
 }) {
 	const slug = (await params).slug;
 	const exercise = await getExerciseByName(desSlugify(slug));
+	console.log(exercise);
 	if (!exercise) {
 		return null;
 	}
@@ -18,7 +19,7 @@ export default async function Page({
 			<div className="container mx-auto">
 				<div className="flex flex-wrap items-center">
 					<div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto">
-						<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-primary-500">
+						<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-primary">
 							<Image
 								alt="Exercise Image"
 								src={exercise.image || DefaultImage}
@@ -31,44 +32,6 @@ export default async function Page({
 									{exercise.name}
 								</h4>
 							</blockquote>
-						</div>
-						<div className="w-full flex flex-wrapitems-start justify-between">
-							{exercise.muscles?.map(
-								(muscle: string, index: number) => {
-									if (muscle == 'legs') {
-										return (
-											<div
-												key={index}
-												className="bg-primary-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full mr-[5px]"
-											></div>
-										);
-									}
-									return null;
-								}
-							)}
-							{exercise.muscle_group?.map(
-								(muscle_group: string, index: number) => {
-									if (muscle_group == 'legs') {
-										return (
-											<div
-												key={index}
-												className="bg-secondary-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full mr-[5px]"
-											></div>
-										);
-									}
-									return null;
-								}
-							)}
-							<div className="flex items-start ml-auto md:mt-0">
-								{exercise.family && exercise.family[0] && (
-									<NavLink
-										href={`/family/${exercise.family[0]}`}
-										className="bg-secondary-500 rounded p-2 hover:bg-secondary-700"
-									>
-										{exercise.family[0]}
-									</NavLink>
-								)}
-							</div>
 						</div>
 					</div>
 					<div className="w-full md:w-6/12 px-4">
