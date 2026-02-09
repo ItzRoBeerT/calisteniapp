18cf0c4a7885d0009dace866b822a0b817631219
RafaelJaime <rajaimor@gmail.com>
2026-02-09 15:33:34 +0100

    feat: update HomePage to display exercise count from API

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,9 @@
 import HomePageClient from '@/components/home/HomePageClient';
+import { getExercises } from '@/actions/exercise';
 
-export default function Home() {
-	return <HomePageClient />;
+export default async function Home() {
+	const exercises = await getExercises();
+	const exerciseCount = exercises?.length ?? 0;
+
+	return <HomePageClient exerciseCount={exerciseCount} />;
 }
