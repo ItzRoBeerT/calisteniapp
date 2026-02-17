2e2e9e441b20e747e01d277d85520ff558fe2d70
RafaelJaime <rajaimor@gmail.com>
2026-02-17 09:47:42 +0100

    feat: update PlaceholderScreen to use string resource for current destination label

--- android/app/src/main/java/com/opencalisthenics/MainActivity.kt
+++ android/app/src/main/java/com/opencalisthenics/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
@@ -76,7 +77,7 @@ fun OpenCalisthenicsApp(
             when (currentDestination) {
                 AppDestinations.EXERCISES -> ExercisesScreen(onExerciseClick = onExerciseClick)
                 AppDestinations.PROFILE -> ProfileScreen(onLogout = onLogout)
-                else -> PlaceholderScreen(currentDestination.label)
+                else -> PlaceholderScreen(stringResource(currentDestination.labelRes))
             }
         }
     }
