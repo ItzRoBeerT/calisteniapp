c3c76ea0b1b212f32f9a39828ef398da372cad44
RafaelJaime <rajaimor@gmail.com>
2026-02-17 11:35:37 +0100

    feat: add status bar padding to ProfileScreen for improved layout

--- android/app/src/main/java/com/opencalisthenics/presentation/user/profile/ProfileScreen.kt
+++ android/app/src/main/java/com/opencalisthenics/presentation/user/profile/ProfileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -33,6 +34,7 @@ fun ProfileScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Background)
+            .statusBarsPadding()
             .verticalScroll(rememberScrollState())
             .padding(horizontal = 16.dp, vertical = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
