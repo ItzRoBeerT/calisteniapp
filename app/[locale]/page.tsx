05e5271e86690e4ecd88dd5b2db72c4685703fa2
RafaelJaime <rajaimor@gmail.com>
2026-03-07 21:11:37 +0100

    refactor(home): replace HomePageClient with HomeClient and remove unused routes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import HomePageClient from '@/components/home/HomePageClient';
+import HomeClient from '@/components/home/HomeClient';
 import { getExercises } from '@/actions/exercise';
 
 export default async function Home() {
@@ -6,5 +6,5 @@ export default async function Home() {
 	const exerciseCount = exercises?.length ?? 0;
 	const featuredExercises = exercises?.slice(0, 6) ?? [];
 
-	return <HomePageClient exerciseCount={exerciseCount} featuredExercises={featuredExercises} />;
+	return <HomeClient exerciseCount={exerciseCount} featuredExercises={featuredExercises} />;
 }
